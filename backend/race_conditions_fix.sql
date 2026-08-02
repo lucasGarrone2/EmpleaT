e1248c32d0cafa26295e46f6b0a2410a7520d2c9
lucasgarrone2 <lucasgarrone2@gmail.com>
2026-08-02 19:16:48 -0300

    chore: dejar de trackear scripts SQL sueltos, agregar .gitignore

--- backend/race_conditions_fix.sql
+++
@@ -1,5 +0,0 @@
--- Añadir restricción UNIQUE compuesta para evitar múltiples postulaciones de una misma persona a un mismo empleo
--- Esto solucionará a nivel de base de datos el problema de Race Conditions generado por clicks rápidos o bots.
-
-ALTER TABLE postulaciones
-ADD CONSTRAINT unique_candidato_oferta UNIQUE (candidato_id, oferta_id);
